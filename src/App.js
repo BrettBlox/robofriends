fb8eaff11d89e0dfeb51c26de2de072919b6f8d9
BrettBlox <Diamond5.12>
2018-11-28 08:55:27 -0700

    ROBOFRIENDSgit a .git a .git a .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
+import './App.css'
 
 class App extends Component {
   state = {
@@ -19,12 +20,12 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    console.log(filteredRobots);
+    console.log(filteredRobots)
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />;
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
