fb37f454afafc9f1fb1d7205dc0214413c7a1ae9
BrettBlox <Diamond5.12>
2018-11-27 20:58:52 -0700

    ROBOTS

---
+++ src/CardList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  const cardArray = robots.map((robot, idx) => {
+    return (
+      <Card
+        key = {idx}
+        id={robots[idx].id}
+        name={robots[idx].name}
+        email={robots[idx].email}
+      />
+    );
+  });
+  return <div>{cardArray}</div>;
+};
+
+export default CardList;
